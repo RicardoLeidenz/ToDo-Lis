99804f324d8d32b7d332265dafa2c60d1532dd11
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-08-19 04:34:08 +0000

    Finished polishing

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,7 +5,7 @@ import ToDoList from "./ToDoList";
 const Home = () => {
 	return (
 		<div className="row justify-content-center">
-			<div className="col-8 text-center bg-dark text-white m-5">
+			<div className="col-5 border rounded text-center bg-dark text-white m-5">
 				<div className="row m-4">
 					<h1>TO DO LIST</h1>
 				</div>
